a196e50d93845712d6e37e445ef8311788f7d6d6
Modyzo <44831870+modyzo@users.noreply.github.com>
2021-07-23 13:41:33 +0300

    Update

--- forum/core/forms.py
+++ forum/core/forms.py
@@ -2,12 +2,14 @@ from django import forms
 from .models import Book
 
 
-class BookForm(forms.ModelForm):
+class BookForm(forms.Form):
     isbn_number = forms.CharField(label="Barcode", required=False)
     name = forms.CharField(label="Book name")
+    description = forms.CharField(label="Short Description")
     class Meta:
         model: Book
         fields = [
             'name',
             'isbn_number',
+            'description',
         ]
