4926226e8c1ece916a54b1a9643b33d4feb1b16c
Modyzo <44831870+modyzo@users.noreply.github.com>
2021-07-24 12:28:44 +0300

    update

--- forum/core/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Book(models.Model):
-    name = models.CharField(max_length=250)
-    isbn_number = models.CharField(max_length=13)
-
-
-    def __str__(self):
-        return self.name
